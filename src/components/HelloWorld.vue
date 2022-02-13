ca59fb6fbde80bf7a58189a7a8f438e1381a89aa
fdeitelhoff <fabian@fabiandeitelhoff.de>
2022-02-13 21:45:49 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+const storeItemUrl = '';
+
+const fetchStoreItems = () => {
+  console.log('fetch store items');
+  fetch(storeItemUrl)
+    .then((stream) => stream.json())
+    .then((data) => (this.items = data))
+    .catch((error) => console.error(error));
+};
+
+const fetchStoreItemsAsync = async () => {
+  console.log('fetch store items async');
+
+  const response = await fetch(storeItemUrl);
+
+  if (!response.ok) {
+    console.log(`An error has occured: ${response.status}`);
+    throw new Error();
+  }
+
+  const data = await response.json();
+};
+
+fetchStoreItems();
+await fetchStoreItemsAsync();
+</script>
+
+<template>
+  <div></div>
+</template>
